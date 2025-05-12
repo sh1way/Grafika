6ebd162addfc4a5ce84ab654d60be595bb49004b
sh1way <fhvygibi@gmail.com>
2025-05-13 01:57:56 +0300

    5lab

---
+++ laba5/laba5/laba5.cpp
@@ -0,0 +1,382 @@
+﻿// laba5.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "laba5.h"
+
+#define MAX_LOADSTRING 100
+
+double movex = 0, movey = 0, scale = 0.01;
+double cre = 0.29824, cim = -0.5942;
+int max_iter = 1024;
+
+double cre_start = -2.0, cre_finish = 1.0;
+double cim_start = -1.5, cim_finish = 1.5;
+
+int scr_x = 500, scr_y = 500;
+double escape_radius = 4.0;
+
+bool drawMandelbrot = false;
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_LABA5, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LABA5));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LABA5));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_LABA5);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Разобрать выбор в меню:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+		{
+			DestroyWindow(hWnd);
+			break;
+		}
+		case VK_NUMPAD8:
+		{
+			movey -= 0.4;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_NUMPAD2:
+		{
+			movey += 0.4;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_NUMPAD6:
+		{
+			movex += 0.4;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_NUMPAD4:
+		{
+			movex -= 0.4;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_NUMPAD7:
+		{
+			movex -= 0.4;
+			movey -= 0.4;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_NUMPAD9:
+		{
+			movex += 0.4;
+			movey -= 0.4;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_NUMPAD3:
+		{
+			movex += 0.4;
+			movey += 0.4;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_NUMPAD1:
+		{
+			movex -= 0.4;
+			movey += 0.4;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_NUMPAD5:
+		{
+			movex = 0;
+			movey = 0;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_ADD:
+		{
+			scale += 0.3;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_SUBTRACT:
+		{
+			scale -= 0.3;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case 'E':
+		{
+			cre += 0.2;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case 'Q':
+		{
+			cre -= 0.2;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case 'A':
+		{
+			cim -= 0.2;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case 'D':
+		{
+			cim += 0.2;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case 'M':
+		{
+			drawMandelbrot = true;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case 'J':
+		{
+			drawMandelbrot = false;
+			InvalidateRect(hWnd, NULL, FALSE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		}
+		break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+			for (int x = 0; x < scr_x; x++)
+			{
+				for (int y = 0; y < scr_y; y++)
+				{
+					double step_x = (cre_finish - cre_start) / scr_x;
+					double step_y = (cim_finish - cim_start) / scr_y;
+
+					double zx = cre_start + x * step_x + movex;
+					double zy = cim_start + y * step_y + movey;
+
+					int iter;
+					if (drawMandelbrot) {
+						double cx = zx;
+						double cy = zy;
+						zx = 0;
+						zy = 0;
+
+						for (iter = 0; iter < max_iter; iter++) {
+							double zx_old = zx;
+							double zy_old = zy;
+
+							zx = zx_old * zx_old - zy_old * zy_old + cx;
+							zy = 2 * zx_old * zy_old + cy;
+
+							if (zx * zx + zy * zy > escape_radius)
+								break;
+						}
+					}
+					else {
+						for (iter = 0; iter < max_iter; iter++) {
+							double zx_old = zx;
+							double zy_old = zy;
+
+							double zx2 = zx_old * zx_old;
+							double zy2 = zy_old * zy_old;
+							double zx3 = zx_old * zx2;
+							double zy3 = zy_old * zy2;
+
+							zx = (zx3 - 3 * zx_old * zy2) + (zx2 - zy2) + 2 * zx_old + cre;
+							zy = (3 * zx2 * zy_old - zy3) + (2 * zx_old * zy_old) + 2 * zy_old + cim;
+
+							if (zx * zx + zy * zy > escape_radius)
+								break;
+						}
+					}
+
+					if (iter == max_iter)
+					{
+						SetPixel(hdc, x, y, RGB(0, 0, 0));
+					}
+					else
+					{
+						SetPixel(hdc, x, y, RGB(iter * 10, iter * 15, iter * 3));
+					}
+
+				}
+			}
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
